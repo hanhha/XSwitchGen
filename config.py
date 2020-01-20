706c3ecffc3aa4178065cb77efdc1c36a57b1051
Hanh Ha <tranhanh.haminh@gmail.com>
2020-01-20 17:06:36 +0700

    Temporary update

--- config.py
+++ config.py
@@ -1,8 +1,48 @@
 prefix            = "N2T"
 n_initiators      = 3
 n_targets         = 5
-datawidth         = 8
-targetid_width    = 3
+addrwidth         = 32
 connection_matrix = [[0, 1, 1, 1, 0],
                      [0, 0, 0, 1, 0],
 										 [1, 0, 0, 1, 1]]
+
+# Number of outstanding requests at initiators
+outstanding_num   = {'I0' : 3,
+										 'I1' : 3,
+										 'I2' : 3}
+
+# Name of agents, must be unique
+agents            = {'I0' : 'CPUm',
+										 'I1' : 'SCRm',
+										 'I2' : 'BRUm',
+										 'T0' : 'CPUs',
+										 'T1' : 'SCRs',
+										 'T2' : 'KBDs',
+										 'T3' : 'RAMs',
+										 'T4' : 'ROMs'}
+
+# [Mask, Value] - Address & Mask == Value => access to this target
+address_map       = {'T0' : ["32'hFFFF_0000", "32'h0001_0000"],
+										 'T1' : ["32'hFFFF_0000", "32'h0002_0000"],
+										 'T2' : ["32'hFFFF_0000", "32'h0003_0000"],
+										 'T3' : ["32'hFFFF_0000", "32'h0004_0000"],
+										 'T4' : ["32'hFFFF_0000", "32'h0005_0000"]}
+
+initiator_addrwidth = {'I0' : 16,
+							         'I1' : 16,
+									     'I2' : 16}
+
+initiator_datawidth = {'I0' : 16,
+							         'I1' : 16,
+									     'I2' : 16}
+
+target_addrwidth = {'T0' : 16,
+							      'T1' : 16,
+							 		  'T2' : 16,
+					    			'T3' : 21,
+										'T4' : 16}
+target_datawidth = {'T0' : 16,
+							      'T1' : 16,
+							 		  'T2' : 16,
+					    			'T3' : 21,
+										'T4' : 16}
--- generator.py
+++ generator.py
@@ -3,49 +3,77 @@
 import os
 import glob
 import shutil
+import math
 import importlib.util as ip
 
 import epy
 
-seed = "Seed"
+class XRouterGen (object):
+	def __init__ (self, env):
+		self.env  = env
+		self.seed = "Seed"
 
-def copy_files (source, dest):
-	for filename in glob.glob(source + '/*.sv'):
-		shutil.copy (filename, dest)
-	for filename in glob.glob(source + '/*.svh'):
-		shutil.copy (filename, dest)
+	def gen_files (self):
+		for filename in glob.glob(self.seed + '/*.epy'):
+			with open (filename,"r") as sf:
+				epython = epy.ePython (sf.read())
+			basename = self.env['prefix'] + os.path.basename (filename)
+			with open (self.env['gendir'] + "/" + os.path.splitext(basename)[0], "w") as f:
+				f.write (epython.render (self.env))
 
-def gen_files (source, dest, env):
-	for filename in glob.glob(source + '/*.epy'):
-		with open (filename,"r") as sf:
-			epython = epy.ePython (sf.read())
-		basename = env['prefix'] + os.path.basename (filename)
-		with open (dest + "/" + os.path.splitext(basename)[0], "w") as f:
-			f.write (epython.render (env))
+	def gen_cmm_files (self):
+		for filename in glob.glob(self.seed + '/*.epy_cmm'):
+			with open (filename,"r") as sf:
+				epython = epy.ePython (sf.read())
+			basename = self.env['cmm_prefix'] + os.path.basename (filename)
+			with open (self.env['gendir'] + "/" + os.path.splitext(basename)[0], "w") as f:
+				f.write (epython.render (self.env))
 
-gen_cfg = ip.spec_from_file_location ('gen_cfg', "./config.py")
-cfg     = ip.module_from_spec (gen_cfg)
-gen_cfg.loader.exec_module (cfg)
+if __name__ == "__main__":
+	gen_cfg = ip.spec_from_file_location ('gen_cfg', "./config.py")
+	cfg     = ip.module_from_spec (gen_cfg)
+	gen_cfg.loader.exec_module (cfg)
 
-env = dict (gendir = cfg.prefix if cfg.prefix != "" else "generated",
-						prefix = cfg.prefix + "_" if cfg.prefix != "" else "",
-						n_initiators   = cfg.n_initiators,
-						n_targets      = cfg.n_targets,
-						datawidth      = cfg.datawidth,
-						targetid_width = cfg.targetid_width,
-						vdw            = cfg.datawidth + cfg.targetid_width,
-						conn_matrix    = cfg.connection_matrix)
+	gendir = cfg.prefix if cfg.prefix != "" else "generated"
 
-print (env['prefix'])
-print (env['n_initiators'])
-print (env['n_targets'])
-print (env['targetid_width'])
-print (env['datawidth'])
-print (env['conn_matrix'])
+	req_env = dict (gendir           = cfg.prefix if cfg.prefix != "" else "generated",
+	                prefix           = cfg.prefix + "_req_" if cfg.prefix != "" else "req_",
+									cmm_prefix       = cfg.prefix + "_" if cfg.prefix != "" else "",
+	                n_initiators     = cfg.n_initiators,
+	                n_targets        = cfg.n_targets,
+									addrwidth        = cfg.addrwidth,
+									agents           = cfg.agents,
+									address_map      = cfg.address_map,
+									target_addrwidth = cfg.target_addrwidth,
+	                conn_matrix      = cfg.connection_matrix)
 
-print ("RTL will be generated in \"%s\" folder." %(env['gendir']))
-shutil.rmtree (env['gendir'], ignore_errors = True)
-os.makedirs   (env['gendir'], exist_ok = True)
+	req_env ['datawidth']      = max(list(cfg.initiator_datawidth.values()))
+	req_env ['pktwidth']       = req_env['datawidth'] + max(list(cfg.target_addrwidth.values()))
+	req_env ['initid_width']   = math.ceil(math.log2(req_env['n_initiators']))
+	req_env ['targetid_width'] = math.ceil(math.log2(req_env['n_targets']))
+	req_env ['vdw']            = req_env ['pktwidth'] + req_env ['initid_width'] + req_env ['targetid_width']
 
-copy_files (seed, env['gendir'])
-gen_files  (seed, env['gendir'], env)
+	rsp_env = dict (gendir = cfg.prefix if cfg.prefix != "" else "generated",
+	                prefix = cfg.prefix + "_rsp_" if cfg.prefix != "" else "rsp_",
+									cmm_prefix = cfg.prefix + "_" if cfg.prefix != "" else "",
+	                n_initiators   = cfg.n_targets,
+	                n_targets      = cfg.n_initiators,
+									agents         = cfg.agents,
+	                conn_matrix    = [*zip(*cfg.connection_matrix)])
+
+	rsp_env ['datawidth']      = max(list(cfg.target_datawidth.values()))
+	rsp_env ['pktwidth']       = rsp_env['datawidth']
+	rsp_env ['initid_width']   = math.ceil(math.log2(rsp_env['n_initiators']))
+	rsp_env ['targetid_width'] = math.ceil(math.log2(rsp_env['n_targets']))
+	rsp_env ['vdw']            = rsp_env ['pktwidth'] + rsp_env ['initid_width'] + rsp_env ['targetid_width']
+
+	req_gen = XRouterGen (req_env)
+	rsp_gen = XRouterGen (rsp_env)
+
+	print ("RTL will be generated in \"%s\" folder." %(gendir))
+	shutil.rmtree (gendir, ignore_errors = True)
+	os.makedirs   (gendir, exist_ok = True)
+
+	req_gen.gen_files ()
+	req_gen.gen_cmm_files ()
+	rsp_gen.gen_files ()
